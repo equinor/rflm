6bd4a8c16db1cf32487953b5c9e406e834fec11a
Timothy-Edward-Kendon <timothy.edward.kendon@gmail.com>
2024-11-29 08:17:43 +0100

    powershell example script

---
+++ runall.ps1
@@ -0,0 +1,4 @@
+python .\rflm_script.py -i ..\sharepoint\data_Lassen2005.txt -o Lassen2005 -v 1 --nlim 1E4 1E8
+python .\rflm_script.py -i ..\sharepoint\data_Stojkovic2018.mat -o Stojkovic2018 -v 1 --nlim 1E4 1E8
+python .\rflm_script.py -i ..\sharepoint\data_Mikulski2022.mat -o Mikulski2022 -v 1 --nlim 1E4 1E8
+python .\rflm_script.py -i ..\sharepoint\DNV_as_welded_filtered.txt -o DNV_as_welded_filtered -v 1 --nlim 1E4 1E8
